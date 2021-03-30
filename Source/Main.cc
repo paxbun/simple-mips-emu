b837fec1fe2eeb253b9f373d3ac6857ad1cc5985
Chanjung Kim <freiyer.paxbun@gmail.com>
2021-03-30 19:10:04 +0900

    Initial commit

---
+++ Source/Main.cc
@@ -0,0 +1,12 @@
+// Copyright (c) 2021 Chanjung Kim (paxbun). All rights reserved.
+// Licensed under the MIT License.
+
+#include <simple-mips-emu/Lib.hh>
+
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    std::cout << Add(2.8, 1.7) << std::endl;
+    return 0;
+}
