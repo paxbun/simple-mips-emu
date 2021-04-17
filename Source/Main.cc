f8864d9bc46aa9524eae0379219db51d545b4114
Chanjung Kim <freiyer.paxbun@gmail.com>
2021-04-17 21:07:23 +0900

    Add sample code

--- Source/Main.cc
+++ Source/Main.cc
@@ -1,9 +1,16 @@
 // Copyright (c) 2021 Chanjung Kim (paxbun). All rights reserved.
 // Licensed under the MIT License.
 
+#include <simple-mips-emu/Memory.hh>
+
 #include <iostream>
 
 int main(int argc, char* argv[])
 {
+    Memory memory { 20, 20 };
+
+    memory.DumpRegisters(std::cout);
+    memory.DumpMemory(std::cout, Address::MakeData(0), Address::MakeData(16));
+
     return 0;
 }
